16a08a87ea23c37b0f356a8bed479f9d3355df9d
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-04-11 14:15:01 +0200

    Kadane's Algorithm
    Used to find maximum subarray sum in an array.

---
+++ Algorithms/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+int maxSubArray(int arr[]);
+
+int main()
+{
+    int arr[9] = {-2, 4, -2, 6, 9, 4, -5, 6, -13};
+
+    int x = maxSubArray(arr);
+
+    std::cout << x;
+
+    return 0;
+}
+
+ int maxSubArray(int arr[9]) {
+        int a = INT_MIN; // Maximum so Far
+        int b = 0;      // Maximum ending at particular poisition it's at (maximum ending here).
+        for(int i = 0; i < 9; i++) {
+            b += arr[i];
+            if(b > a){
+                a = b;
+            }
+            if(b < 0) {
+                b = 0;
+            }
+        }
+    return a;
+    }
