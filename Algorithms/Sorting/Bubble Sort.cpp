f169f9b1a1691bbd1d3734a79583e4754ab40b23
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-04-11 23:08:14 +0200

    Bubble Sort
    One of many sorting algorithms. It runs in quadratic O(n^2) time.

--- Algorithms/Sorting/Bubble Sort.cpp
+++ Algorithms/Sorting/Bubble Sort.cpp
@@ -1,26 +1,26 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h>        // Includes everything needed with one line.
 
 void swap(int a, int b);
 void bubbleSort(int arr[]);
 
-int main()
+int main()              // Test Driver for the algorithm
 {
     int arr[9] = {3, 8, 1, 2, 10, 5, 15, 9, 4};
 
     for(int f = 0; f < 9; f++){
-        std::cout << arr[f] << std::endl;
+        std::cout << arr[f] << std::endl;       // Before swap
     }
     std::cout << "" << std::endl;
 
     bubbleSort(arr);
 
-    for(int k = 0; k < 9; k++){
+    for(int k = 0; k < 9; k++){                 // After Swap
         std::cout << arr[k] << std::endl;
     }
     return 0;
 }
 
- void bubbleSort(int arr[]){
+ void bubbleSort(int arr[]){            // The actual algorithm
     for(int i = 0; i < 9; i++){
         for(int j = 0; j < 8; j++){
             if(arr[j] > arr[j + 1]){
