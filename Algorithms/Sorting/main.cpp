f9d1c6b802f67d83e7df2b4317086966fc90a06f
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-07-27 22:25:38 +0200

    Insertion Sort
    Time Complexity - O(n^2) || Space Complexity - O(1).

---
+++ Algorithms/Sorting/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void insertionSort(int arr[], size_t n);
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int arr[] = {1, 10, 2, 9, 3, 8, 4, 5, 8, 6, 7};
+    size_t n = sizeof(arr)/sizeof(arr[0]);
+    insertionSort(arr, n);
+
+    for(unsigned int i = 0; i < n; i++){
+        std::cout << arr[i] << "\t";
+    }
+
+}
+
+void insertionSort(int arr[], size_t n){
+   int i, k, j;
+   for(i = 1; i < n; i++){
+        j = i - 1;
+        k = arr[i];
+        while(j > 0 && arr[j] > k){
+            arr[j+1] = arr[j]; // j+1 is where k is.
+            j--;
+        }
+        arr[j+1] = k;
+   }
+}
