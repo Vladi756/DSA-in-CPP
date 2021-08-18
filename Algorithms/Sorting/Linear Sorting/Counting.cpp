f2ddf6b146ae92d605d97545ce49d37a3861c1cf
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-08-18 14:07:32 +0200

    Count Sort
    Time Complexity - O(n + k) || Space Complexity - O(n + k)

--- Algorithms/Sorting/Linear Sorting/Counting.cpp
+++ Algorithms/Sorting/Linear Sorting/Counting.cpp
@@ -1,31 +1,32 @@
 #include <bits/stdc++.h>
 
-void countingSort(int arr[], size_t n, int k){
-    int count[k+1];
-    int b[n];
-    for(int i = 0; i < n; i++){     // Initialize everything to 0.
-        count[i] = 0;
-    }
-    for(int i = 0; i < n; i++){     // Counts number of elements with distinct keys.
-        ++count[arr[i]];
-    }
-    for(int i = 1; i <= k; i++){    // Updates the count array to show the position (NOT index position) the values should be at.
-        count[i] += count[i-1];
-    }
-    for(int i = n-1; i >= 0; i--){  // Prints the numbers to their respective positions.
-        b[--count[arr[i]]] = arr[i];
-    }
-    for(int i = 0; i < n; i++){     // Copies sorted result back into original array.
-        arr[i] = b[i];
-    }
+void countSort(std::vector<int> &a, int n, int k);
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-int main(){ // Driver Code 
-    int arr[] = {0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5};
-    size_t n = 11;
-    int k = 10;
-    countingSort(arr, n, k);
-    for(int x : arr){std::cout << arr[x] << "\t";}
+int main(){
+    std::vector<int> nums = {0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5};
+    int n = nums.size();
+    countSort(nums, n, 10);
+
+    for(int x : nums){std::cout << nums[x] << "\t";}
+}
+
+void countSort(std::vector<int> &a, int n, int k){
+    std::vector<int> count(k+1, 0);
+    std::vector<int> temp(n, 0);
+
+    for(int i = 0; i < n; i++){count[a[i]]++;}
+
+    for(int i = 1; i <= k; i++){count[i] += count[i-1];}
+
+    for(int i = n-1; i >= 0; i--){ temp[--count[a[i]]] = a[i];} // PREFIX minus - remember. 
+
+    for(int i = 0; i < n; i++){a[i] = temp[i];}
 }
 
 // Output: 0    1   2   3   4   5   6   7   8   9   10
